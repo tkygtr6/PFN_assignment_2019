90d6f43e1882be14e097f44a07538d8b9d16195e
tkygtr6 <tkygtr6@gmail.com>
2019-05-04 03:29:22 +0900

    fix

--- worker/main.cpp
+++ worker/main.cpp
@@ -18,7 +18,7 @@ void update_jobs(){
     std::list<int> finish_job_ids;
     for(auto it = job_list.begin(); it != job_list.end(); ++it){
         auto res = it->update_task();
-        if(res.first){
+        if(!res){
             it = job_list.erase(it);
         }
     }
