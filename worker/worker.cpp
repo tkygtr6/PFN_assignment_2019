affe42ea5a5f70092fdeb459cb1d0787962a01fa
tkygtr6 <tkygtr6@gmail.com>
2019-05-03 22:41:59 +0900

    Solve 1-2 except for the remove of finished job

--- worker/worker.cpp
+++ worker/worker.cpp
@@ -28,6 +28,35 @@ class Job{
         int job_id;
         int priority;
         std::vector<int> tasks;
+        int num_tasks;
+        int task_no;
+        int remaining_point;
+
+        Job(json11::Json job_json){
+            created = job_json["Created"].int_value();
+            job_id = job_json["JobID"].int_value();
+            priority = job_json["Priority"].int_value();
+            for(const auto& task : job_json["Tasks"].array_items()){
+                tasks.push_back(task.int_value());
+            }
+            num_tasks = tasks.size();
+            task_no = 1; // begin from 1
+            remaining_point = tasks[task_no - 1];
+        }
+
+        void update(){
+            if (remaining_point > 1){
+                remaining_point--;
+            }else{
+                if(task_no == num_tasks){
+                    remaining_point = 0;
+                    // remove
+                }else{
+                    task_no++;
+                    remaining_point = tasks[task_no - 1];
+                }
+            }
+        }
 };
 
 std::vector<Job> get_and_parse_json(int sec){
@@ -58,56 +87,38 @@ std::vector<Job> get_and_parse_json(int sec){
     auto jobs_json = json11::Json::parse(res, json_err); 
     jobs_json = jobs_json["Jobs"];
 
-    // convert Json to Job
     std::vector<Job> jobs;
     for(const auto& job_json : jobs_json.array_items()){
-        Job job;
-        job.created = job_json["Created"].int_value();
-        job.job_id = job_json["JobID"].int_value();
-        job.priority = job_json["Priority"].int_value();
-        for(const auto& task : job_json["Tasks"].array_items()){
-            job.tasks.push_back(task.int_value());
-        }
+        Job job(job_json);
         jobs.push_back(job);
     }
     return jobs;
 }
 
-/*
-class task{
-    public:
-        int task_point;
-        int priority;
-        int job_id;
-        int job_r;
-        int task_no;
-};
+std::vector<Job> job_list;
 
-std::vector<json11::Json> job_list;
-std::vector<task> ready_queue;
-*/
-
-/*
-void append_new_job(Job new_job){
-    job_list.push_back(new_job);
-
-    new_task = new_job[]
-    ready_queue.push_back();
+void calc_exec_point(){
+    int exec_point = 0;
+    for(const auto& job : job_list){
+        exec_point += job.remaining_point;
+    }
+    std::cout << exec_point << std::endl;
 }
-*/
+
+void update_jobs(){
+    for(auto& job : job_list){
+        job.update();
+    }
+}
 
 int main(){
-    for(int i = 0; i <= 1000; i++){
+    for(int i = 0; i <= 10000; i++){
         std::vector<Job> new_jobs = get_and_parse_json(i);
         for(const auto& new_job : new_jobs){
-            std::cout << i << " " << new_job.job_id << std::endl;
-            //append_new_job(new_job);
+            job_list.push_back(new_job);
         }
-       
-        /*
-        update_exec_list();
-        execute_task();
-        */
+        calc_exec_point();
+        update_jobs();
     }
 
     return 0;
