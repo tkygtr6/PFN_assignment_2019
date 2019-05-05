6fa39d3ee7cc984c39db69a719f62321fd6e3cb0
tkygtr6 <tkygtr6@gmail.com>
2019-05-06 02:25:05 +0900

    Update test_run.sh

--- test/test_run.sh
+++ test/test_run.sh
@@ -4,13 +4,19 @@ cd ../server
 DATA_DIR=../test/data_test1 FLASK_APP=server.py flask run 1>/dev/null 2>/dev/null &
 cd ../worker
 #make clean
-#make
+make
 sleep 2
 
-diff <(MAXTIME=20 CAPACITY=-1 PRIORITY_RANGE=1 ./worker | awk '{print $2}') <(cat ../test/ans/test1 | awk '{print $2}') | wc -l
+echo "test1"
+diff <(MAXTIME=20 CAPACITY=-1 PRIORITY_RANGE=1 ./worker | awk '{print $2}') <(cat ../test/ans/test1 | awk '{print $2}') 
 
-diff <(MAXTIME=20 CAPACITY=10 PRIORITY_RANGE=1 ./worker | awk '{print $2}') <(cat ../test/ans/test2 | awk '{print $2}') | wc -l
+echo "test2"
+diff <(MAXTIME=20 CAPACITY=10 PRIORITY_RANGE=1 ./worker | awk '{print $2}') <(cat ../test/ans/test2 | awk '{print $2}')
 
-diff <(MAXTIME=20 CAPACITY=10 PRIORITY_RANGE=2 ./worker | awk '{print $2}') <(cat ../test/ans/test3 | awk '{print $2}') | wc -l
+echo "test3"
+diff <(MAXTIME=20 CAPACITY=10 PRIORITY_RANGE=2 NAIVE=1 ./worker | awk '{print $2}') <(cat ../test/ans/test3 | awk '{print $2}') 
+
+echo "test4"
+diff <(MAXTIME=20 CAPACITY=10 PRIORITY_RANGE=2 ./worker | awk '{print $2}') <(cat ../test/ans/test3 | awk '{print $2}')
 
 kill $(ps -a | grep "flask run" | grep -v grep | awk '{print $1}')
