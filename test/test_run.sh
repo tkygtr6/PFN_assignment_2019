7d5d6a01fea0d1f94d3fc5fae0c3481e50e34d57
tkygtr6 <tkygtr6@gmail.com>
2019-05-06 07:21:22 +0900

    Add data_test3

--- test/test_run.sh
+++ test/test_run.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
+COUNT=1
 
-#cd ../server
-#DATA_DIR=../test/data_test1 FLASK_APP=server.py flask run 1>/dev/null 2>/dev/null &
-#cd ../worker
-##make clean
-#make
-#sleep 2
+cd ../server
+DATA_DIR=../test/data_test1 FLASK_APP=server.py flask run 1>/dev/null 2>/dev/null &
+cd ../worker
+#make clean
+make
+sleep 2
 
-#for i in `seq 5`
-#do
+for i in `seq $COUNT`
+do
 
-#echo "test1"
-#diff <(MAXTIME=20 CAPACITY=-1 PRIORITY_RANGE=1 ./worker 2>/dev/null | awk '{print $2}') <(cat ../test/ans/test1_1 | awk '{print $2}') 
+echo "test1"
+diff <(MAXTIME=20 CAPACITY=-1 PRIORITY_RANGE=1 ./worker 2>/dev/null | awk '{print $2}') <(cat ../test/ans/test1_1 | awk '{print $2}') 
 
-#echo "test2"
-#diff <(MAXTIME=20 CAPACITY=10 PRIORITY_RANGE=1 ./worker 2>/dev/null | awk '{print $2}') <(cat ../test/ans/test1_2 | awk '{print $2}')
+echo "test2"
+diff <(MAXTIME=20 CAPACITY=10 PRIORITY_RANGE=1 ./worker 2>/dev/null | awk '{print $2}') <(cat ../test/ans/test1_2 | awk '{print $2}')
 
-#echo "test3"
-#diff <(MAXTIME=20 CAPACITY=10 PRIORITY_RANGE=2 NAIVE=1 ./worker 2>/dev/null | awk '{print $2}') <(cat ../test/ans/test1_3 | awk '{print $2}') 
+echo "test3"
+diff <(MAXTIME=20 CAPACITY=10 PRIORITY_RANGE=2 NAIVE=1 ./worker 2>/dev/null | awk '{print $2}') <(cat ../test/ans/test1_3 | awk '{print $2}') 
 
-#echo "test4"
-#diff <(MAXTIME=20 CAPACITY=10 PRIORITY_RANGE=2 ./worker 2>/dev/null | awk '{print $2}') <(cat ../test/ans/test1_4 | awk '{print $2}')
+echo "test4"
+diff <(MAXTIME=20 CAPACITY=10 PRIORITY_RANGE=2 ./worker 2>/dev/null | awk '{print $2}') <(cat ../test/ans/test1_4 | awk '{print $2}')
 
-#done
+done
 
-#kill $(ps -a | grep "flask run" | grep -v grep | awk '{print $1}')
+kill $(ps -a | grep "flask run" | grep -v grep | awk '{print $1}')
 
 cd ../server
 DATA_DIR=../test/data_test2 FLASK_APP=server.py flask run 1>/dev/null 2>/dev/null &
@@ -32,6 +33,9 @@ sleep 2
 
 cd ../worker
 make
+
+for i in `seq $COUNT`
+do
 
 echo "test2_1"
 diff <(MAXTIME=40 CAPACITY=-1 PRIORITY_RANGE=1 ./worker 2>/dev/null | awk '{print $2}') <(cat ../test/ans/test2_1 | awk '{print $2}') 
@@ -44,6 +48,28 @@ diff <(MAXTIME=40 CAPACITY=8 PRIORITY_RANGE=2 NAIVE=1 ./worker 2>/dev/null | awk
 
 echo "test2_4"
 diff <(MAXTIME=40 CAPACITY=8 PRIORITY_RANGE=2 ./worker 2>/dev/null | awk '{print $2}') <(cat ../test/ans/test2_4 | awk '{print $2}') 
+
+done
+
+kill $(ps -a | grep "flask run" | grep -v grep | awk '{print $1}')
+
+cd ../server
+DATA_DIR=../test/data_test3 FLASK_APP=server.py flask run 1>/dev/null 2>/dev/null &
+sleep 2
+
+cd ../worker
+make
+
+for i in `seq $COUNT`
+do
+
+echo "test3_3"
+diff <(MAXTIME=40 CAPACITY=10 PRIORITY_RANGE=2 NAIVE=1 ./worker 2>/dev/null | awk '{print $2}') <(cat ../test/ans/test3_3 | awk '{print $2}') 
+
+echo "test3_4"
+diff <(MAXTIME=40 CAPACITY=10 PRIORITY_RANGE=2 ./worker 2>/dev/null | awk '{print $2}') <(cat ../test/ans/test3_4 | awk '{print $2}') 
+
+done
 
 kill $(ps -a | grep "flask run" | grep -v grep | awk '{print $1}')
 
